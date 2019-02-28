1f87e209f8e1afd4ebb635758ceccba3ac0b2e02
Ezzeddine El Sai <ezzeddinesai@hotmail.com>
2019-02-28 13:32:59 -0700

    Implimentation of the basic tests in the cryptography handbook.

---
+++ Randomness_tests.py
@@ -0,0 +1,133 @@
+import math
+from scipy import stats
+import itertools
+
+significance = 0.05
+
+def serial(seq):
+    if len(seq) < 21:
+        return "error, sequence too small."
+    n1 = seq.count(1)
+    n0 = seq.count(0)
+    n00 = 0
+    n01 = 0
+    n10 = 0
+    n11 = 0
+    for i in range(len(seq)-1):
+        subseq = str(seq[i])+str(seq[i+1])
+        if (subseq == "00"):
+            n00 += 1
+        elif(subseq == "01"):
+            n01 += 1
+        elif(subseq == "10"):
+            n10 += 1
+        elif(subseq == "11"):
+            n11 += 1
+    statistic = ((4/(len(seq)-1.0))*((n00**2)+(n01**2)+(n10**2)+(n11**2)))-((2*((n0**2)+(n1**2)))/(len(seq)+0.0))+1
+    if (1 - stats.chi2.cdf(statistic, 2)) < significance:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, 2)))
+              +", likely fasle.")
+    else:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, 2)))
+              +", likely true.")
+
+
+#length of sequence must be >9
+#chi square deg 1, non random is higher (one sided test)
+def monobit(seq):
+    global significance 
+    n_0 = 0
+    n_1 = 0
+    n = len(seq)
+    for bit in seq:
+        if bit == 0:
+            n_0 += 1
+        else:
+            n_1 += 1
+    statistic = ((n_0-n_1)**2)/float(n)
+    if (1 - stats.chi2.cdf(statistic, 1)) < significance:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, 1)))
+              +", likely fasle.")
+    else:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, 1)))
+              +", likely true.")
+
+def poker(seq,m):
+    global significance
+    k = math.floor(len(seq)/(m+0.0))
+    if k < 5*(2**m):
+        return "error, invalid m parameter."
+    counter = [0 for i in range(2**m)]
+    comb = list(itertools.product([0, 1], repeat=m))
+    for i in range(k):
+        tup = ()
+        for j in range(m):
+            tup = tup+(seq[(m*i)+j],)
+        #tup = (seq[i],seq[(i)+1],seq[(i)+2],seq[(i)+3])
+        for j in range(2**m):
+            if (tup == comb[j]):
+                counter[j] += 1
+                break
+    sum_counter = 0
+    for i in range(2**m):
+        sum_counter += (counter[i]**2)
+    statistic = (((2**m)/(k+0.0))*(sum_counter))-k
+    if (1 - stats.chi2.cdf(statistic, (2**m)-1)) < significance:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, (2**m)-1)))
+              +", likely fasle.")
+    else:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, (2**m)-1)))
+              +", likely true.")
+
+def runs(seq):
+    n = len(seq)
+    gap = 0
+    block = 0
+    zero = True
+    k = 1
+    while(True):
+        e = (n-k+1+3)/(2**(k+1+2))
+        if e >= 5:
+            k += 1
+        else:
+            break
+    e = (n-k+3)/(2**(k+2))  
+    gap_counts = [0 for i in range(n)]
+    block_counts = [0 for i in range(n)]
+    for bit in seq:
+        if ((zero == True) and (bit == 1)):
+            zero = False
+            if gap > 0:
+                gap_counts[gap] += 1
+                gap = 0
+        if ((zero == False) and (bit == 0)):
+            zero = True
+            if block > 0:
+                block_counts[block] += 1
+                block = 0
+        if bit == 0:
+            gap += 1
+        else:
+            block += 1
+            zero = False
+    if gap > 0:
+        gap_counts[gap] += 1
+    if block > 0:
+        block_counts[block] += 1
+    sum_blocks = 0
+    sum_gaps = 0
+    for i in range(1,k+1):
+        e_i = (n-i+3)/(2**(i+2))
+        sum_blocks += ((block_counts[i] - e_i)**2)/e_i
+        sum_gaps+= ((gap_counts[i] - e_i)**2)/e_i
+
+    statistic = sum_blocks + sum_gaps
+    if (1 - stats.chi2.cdf(statistic, (2*k)-2)) < significance:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, (2*k)-2)))
+              +", likely fasle.")
+    else:
+        print("Statistic = "+str(statistic)+", Signficance = "+str((1 - stats.chi2.cdf(statistic, (2*k)-2)))
+              +", likely true.")
+
+
+test = [1,1,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,1,0,0,1,1,1,0,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,1]*4
